fb78b3607b5cbc0903548f75a0fa297376a40fb9
ramsey6340 <eniac5project@gmail.com>
2024-05-29 10:29:48 +0200

    v1.0.0 first commit

---
+++ src/main/java/com/smartech/gesco_ui/GescoUiApplication.java
@@ -0,0 +1,13 @@
+package com.smartech.gesco_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GescoUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GescoUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartech/gesco_ui/GescoUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartech.gesco_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GescoUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
